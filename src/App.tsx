29d802cb7f11db5501ad181e0616abb8f7df8d14
Stanley Kurniawan <terserahskurniawan@gmail.com>
2024-03-03 11:01:26 -0800

    changes for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import { getJobs, searchJobs } from "./server";
 function App() {
   const [internalData, setInternalData] = useState([] as Job[]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const jobs = await getJobs();
       setInternalData(jobs);
+      setLoading(false);
     })();
   }, []);
 
@@ -38,7 +39,7 @@ function App() {
         <input
           type="text"
           className="w-1/2 rounded-l-lg p-3 font-sans focus:outline-none"
-          placeholder="What job do you want to look for?"
+          placeholder="Search a job..."
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
@@ -49,10 +50,16 @@ function App() {
           {loading ? "loading..." : "search"}
         </span>
       </div>
-      <div className="flex justify-center bg-slate-200 py-2 font-sans">
-        showing <span className="px-1 font-bold">{internalData.length}</span>{" "}
-        jobs
-      </div>
+      {internalData.length ? (
+        <div className="flex justify-center bg-slate-200 py-2 font-sans">
+          showing <span className="px-1 font-bold">{internalData.length}</span>{" "}
+          jobs
+        </div>
+      ) : (
+        <div className="flex justify-center bg-slate-200 py-2 font-sans">
+          loading...
+        </div>
+      )}
       <div className="divide-y px-20 *:py-10 max-md:px-10">
         {internalData.map((item, index) => {
           return (
